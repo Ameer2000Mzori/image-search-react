a93194c0b7d65d5d374e9d378f83d644d2e8ee25
Ameer Ameen <ameerapex496@gmail.com>
2024-02-14 02:42:15 +0100

    added more logic to the button and trying to fetch data from the api

--- src/component/Home.jsx
+++ src/component/Home.jsx
@@ -1,12 +1,29 @@
+import { useState } from 'react'
 import Images from './Images'
 
 const Home = () => {
-  // fetch api
+  // use state
+    const [images, setImages] = useState([])
+  const [searchTerm, setSearchTerm] = useState('')
+//   fetch api
+    const getImages = async ()=>{
+      try{
+          const res = await
+      }
+    }
+
   return (
     <>
       <div className="fixed h-[40px] w-[100%] top-0 left-0 bg-zinc-700 flex flex-row items-center justify-center text-center gap-8">
         <input type="text" placeholder="search something..." />
-        <button className="w-[150px] h-[30px] rounded-md bg-zinc-400 hover:bg-zinc-500 text-white active:bg-zinc-600 ">
+        <button
+            onClick={getImages}
+          className="w-[150px] h-[30px] rounded-md bg-zinc-400 hover:bg-zinc-500 text-white active:bg-zinc-600 "
+          value={searchTerm}
+          onChange={() => {
+            setSearchTerm(searchTerm)
+          }}
+        >
           submit
         </button>
       </div>
