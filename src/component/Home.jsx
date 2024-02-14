a4b5d51314bc87a0f3be8f0b96afc5528567eeb0
Ameer Ameen <ameerapex496@gmail.com>
2024-02-14 02:35:14 +0100

    added styling for the form nav

--- src/component/Home.jsx
+++ src/component/Home.jsx
@@ -1,7 +1,17 @@
 import React from 'react'
 
 const Home = () => {
-  return <div>Home</div>
+  return (
+    <>
+      <div className="fixed h-[40px] w-[100%] top-0 left-0 bg-zinc-700 flex flex-row items-center justify-center text-center gap-8">
+        <input type="text" placeholder="search something..." />
+        <button className="w-[150px] h-[30px] rounded-md bg-zinc-400 hover:bg-zinc-500 text-white active:bg-zinc-600 ">
+          submit
+        </button>
+      </div>
+      <div></div>
+    </>
+  )
 }
 
 export default Home
