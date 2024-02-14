325d6c2f0c6e8ce4dd303733a8b47886a4a35a21
Ameer Ameen <ameerapex496@gmail.com>
2024-02-14 03:00:48 +0100

    working on api

--- src/component/Home.jsx
+++ src/component/Home.jsx
@@ -1,28 +1,30 @@
-import { useState } from 'react'
+import React, { useState, useEffect } from 'react'
+
 import Images from './Images'
 
 const Home = () => {
   // use state
-    const [images, setImages] = useState([])
   const [searchTerm, setSearchTerm] = useState('')
-//   fetch api
-    const getImages = async ()=>{
-      try{
-          const res = await
-      }
-    }
+
+  let API_KEY = `5__hOrhuSvuNlKzYw0zWUYLEk7Omf0A93siCEPW7bSA`
+  const getImages = () => {
+    console.log(`Searching for images of: ${searchTerm}`)
+  }
 
   return (
     <>
       <div className="fixed h-[40px] w-[100%] top-0 left-0 bg-zinc-700 flex flex-row items-center justify-center text-center gap-8">
-        <input type="text" placeholder="search something..." />
-        <button
-            onClick={getImages}
-          className="w-[150px] h-[30px] rounded-md bg-zinc-400 hover:bg-zinc-500 text-white active:bg-zinc-600 "
+        <input
+          type="text"
+          placeholder="search something..."
           value={searchTerm}
-          onChange={() => {
-            setSearchTerm(searchTerm)
+          onChange={(e) => {
+            setSearchTerm(e.target.value)
           }}
+        />
+        <button
+          onClick={getImages}
+          className="w-[150px] h-[30px] rounded-md bg-zinc-400 hover:bg-zinc-500 text-white active:bg-zinc-600 "
         >
           submit
         </button>
